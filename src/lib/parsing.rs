048cfbd8ab60c9cc1d741798ec3fedd49c27edf6
newo-2001 <o.elderbroek@gmail.com>
2023-08-19 21:22:37 +0200

    2015 Day 25

--- src/lib/parsing.rs
+++ src/lib/parsing.rs
@@ -4,11 +4,12 @@ use nom::{
     Parser,
     IResult,
     combinator::{value, eof},
-    error::ParseError,
+    error::{ParseError, VerboseError},
     branch::alt,
-    character::complete::{crlf, newline},
-    Slice, Compare, InputIter, InputLength, AsChar,
+    character::complete::{crlf, newline, anychar},
+    Slice, Compare, InputIter, InputLength, AsChar, multi::many_till,
 };
+use tupletools::snd;
 
 pub fn ignore<I, O, E, F>(parser: F) -> impl FnMut(I) -> IResult<I, (), E>
     where E : ParseError<I>,
@@ -25,4 +26,19 @@ pub fn optional_newline<I, E>(input: I) -> IResult<I, (), E>
         ignore(newline),
         ignore(eof)
     ))(input)
+}
+
+pub fn skip_until<I, O, E, F, C>(parser: F) -> impl Parser<I, O, E>
+    where E: ParseError<I>,
+          F: Parser<I, O, E>,
+          I: Clone + InputIter<Item = C> + Slice<RangeFrom<usize>> + InputLength,
+          C: AsChar
+{
+    many_till(anychar, parser).map(snd)
+}
+
+pub fn run<'a, O, F>(parser: &mut F, input: &'a str) -> Result<O, String>
+    where F: Parser<&'a str, O, VerboseError<&'a str>>
+{
+    Ok(parser.parse(input).map_err(|err| err.to_string())?.1)
 }
