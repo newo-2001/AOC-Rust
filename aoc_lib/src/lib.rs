1a4be162fea620024d10923f2c0fdbe42919a33e
newo-2001 <o.elderbroek@gmail.com>
2023-08-23 00:08:40 +0200

    Deprecate spatial module

--- aoc_lib/src/lib.rs
+++ aoc_lib/src/lib.rs
@@ -1,6 +1,7 @@
 #![feature(generators)]
 #[macro_use] extern crate impl_ops;
 
+#[deprecated(note="Use the more abstract `geometry` module instead")]
 pub mod spatial;
 pub mod parsing;
 pub mod math;
