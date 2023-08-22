92ae7b1ab4bef6612916f084a75014413599dd55
newo-2001 <o.elderbroek@gmail.com>
2023-08-23 00:18:10 +0200

    Improve 2015 Day 4

--- aoc_solvers/src/aoc_2015/aoc_2015_day_04.rs
+++ aoc_solvers/src/aoc_2015/aoc_2015_day_04.rs
@@ -1,20 +1,17 @@
+use aoc_lib::math::natural_numbers;
 use aoc_runner_api::SolverResult;
+use hex::ToHex;
 use md5;
 
-fn proof_of_work(zeros: usize, prefix: &str) -> u32 {
-    let mut i = 0;
-    let hash_prefix = "0".repeat(zeros);
+fn proof_of_work(zeroes: usize, prefix: &str) -> u32 {
+    let hash_prefix = "0".repeat(zeroes);
 
-    loop {
-        let key = prefix.to_owned() + i.to_string().as_str();
-        let hash = format!("{:x}", md5::compute(key.as_bytes()));
-        if hash.starts_with(&hash_prefix) {
-            break;
-        }
-        i += 1;
-    }
-    
-    i   
+    natural_numbers::<u32>(0)
+        .find_map(|index| {
+            let key = format!("{}{}", prefix, index);
+            let hash: String = md5::compute(key).encode_hex();
+            hash.starts_with(&hash_prefix).then_some(index)
+        }).unwrap()
 }
 
 pub fn solve_part_1(input: &str) -> SolverResult {
