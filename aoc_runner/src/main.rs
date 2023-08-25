2a4b5c9c5119a09c20933935736b586233e27620
newo-2001 <o.elderbroek@gmail.com>
2023-08-25 13:25:19 +0200

    Add colors to runner output

--- aoc_runner/src/main.rs
+++ aoc_runner/src/main.rs
@@ -1,6 +1,7 @@
 use std::{error::Error, env};
 
 use aoc_runner_api::{Puzzle, SolverResult};
+use colored::{Colorize, Color};
 use run::{RunnerAction, run_puzzle, verify_puzzle};
 
 mod arguments;
@@ -27,14 +28,19 @@ fn execute<'a>(runner: fn(&Puzzle) -> SolverResult, puzzles: &Vec<Puzzle>) {
     let results = puzzles.into_iter().map(|puzzle| {
         let (year, day, part) = (puzzle.year, puzzle.day, puzzle.part);
         let result = runner(puzzle);
+        let puzzle = format!("[{} day {:02} part {}]", year, day, part).bright_yellow().bold();
         match &result {
-            Ok(answer) => println!("[{} day {:02} part {}] (Success) {}", year, day, part, answer),
-            Err(err) => println!("[{} day {:02} part {}] (Failed) {}", year, day, part, err.to_string())
+            Ok(answer) => println!("{} {} {}", puzzle, "(Success)".bright_green().bold(), answer),
+            Err(err) => println!("{} {} {}", puzzle, "(Failure)".bright_red().bold(), err.to_string().bright_red())
         }
 
         result
     });
 
     let passed = results.filter(Result::is_ok).count();
-    println!("{} / {} puzzles executed successfully", passed, puzzles.len());
+    let msg = format!("{} / {} puzzles executed successfully", passed, puzzles.len())
+        .color(if passed == puzzles.len() { Color::BrightGreen } else { Color::BrightRed })
+        .bold();
+
+    println!("{}", msg);
 }
