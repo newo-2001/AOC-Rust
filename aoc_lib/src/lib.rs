4456ba0c82a6598750b5028255a110cc4993b643
newo-2001 <o.elderbroek@gmail.com>
2023-08-29 18:08:29 +0200

    Refactor 2015 day 18 to work with new geometry api

--- aoc_lib/src/lib.rs
+++ aoc_lib/src/lib.rs
@@ -1,8 +1,5 @@
 #![feature(generators, step_trait)]
-#[macro_use] extern crate impl_ops;
 
-#[deprecated(note="Use the more abstract `geometry` module instead")]
-pub mod spatial;
 pub mod parsing;
 pub mod math;
 pub mod geometry;
--- aoc_solvers/src/aoc_2015/aoc_2015_day_18.rs
+++ aoc_solvers/src/aoc_2015/aoc_2015_day_18.rs
@@ -1,91 +1,74 @@
 use std::error::Error;
 
-use aoc_lib::spatial::{Grid, Coordinate, Area};
+use aoc_lib::{parsing::{Runnable, parse_lines}, geometry::{Dimensions, Point2D, GridView, Grid, GridViewMut}, functional::repeat_apply};
 use aoc_runner_api::SolverResult;
-use itertools::Itertools;
-use tupletools::snd;
+use nom::{combinator::value, Parser, character::complete, multi::many0};
 
-fn parse_cell(char: char) -> Result<bool, Box<dyn Error>> {
-    match char {
-        '.' => Ok(false),
-        '#' => Ok(true),
-        c => Err(Box::from(format!("Grid contained unexpected character: '{}'", c)))
+fn neighbours_on(grid: &GridView<bool>, cell: Point2D<usize>) -> usize {
+    cell.neighbours()
+        .filter_map(|location| grid.get(location)?.then_some(()))
+        .count()
+}
+
+fn next_state(grid: GridView<bool>) -> Grid<bool> {
+    grid.enumerate_map(|(location, &state)| {
+        match (state, neighbours_on(&grid, location)) {
+            (true, 2) | (true, 3) => true,
+            (false, 3) => true,
+            _ => false
+        }
+    })
+}
+
+fn fix_corners(grid: &mut GridViewMut<bool>) {
+    let corners = grid.area().corners();
+
+    for location in corners {
+        *grid.get_mut(location).unwrap() = true;
     }
 }
 
-fn neighbours(grid: &Grid<bool>, cell: &Coordinate) -> usize {
-    cell.neighbours()
-        .into_iter()
-        .filter_map(|neighbour| grid.value_at(&neighbour))
+fn count_lights(grid: &GridView<bool>) -> usize {
+    grid.into_iter()
         .filter(|&x| *x)
         .count()
 }
 
-fn next_state<'a>(grid: &'a Grid<bool>, area: Area) -> impl Iterator<Item=(Coordinate, bool)> + 'a {
-    grid.iter(area).map(|(coord, state)| {
-        let state = state.expect("Grid index out of range");
-        let neighbours = neighbours(grid, &coord);
-
-        let new_state = match (state, neighbours) {
-            (true, 2) | (true, 3) => true,
-            (false, 3) => true,
-            _ => false
-        };
-
-        (coord, new_state)
-    })
-}
-
-fn animate(grid: &mut Grid<bool>, area: Area) {
-    let new_state = next_state(grid, area).collect_vec();
-    for (coord, state) in new_state {
-        grid.squares.insert(coord, state);
-    }
-}
-
-fn fix_corners(grid: &mut Grid<bool>, area: &Area) {
-    let corners = area.corners();
+fn parse_line(input: &str) -> Result<Vec<bool>, String> {
+    let cell = value(true, complete::char('#'))
+        .or(value(false, complete::char('.')));
     
-    for coord in corners {
-        grid.squares.insert(coord, true);
-    }
+    many0(cell).run(input)
 }
 
-fn animate_broken_corners(grid: &mut Grid<bool>, area: Area) {
-    fix_corners(grid, &area);
-    animate(grid, area);
-}
+fn parse_grid(input: &str) -> Result<Grid<bool>, Box<dyn Error>> {
+    let data = parse_lines(parse_line, input)?
+        .into_iter()
+        .flatten();
 
-fn count_lights(grid: &Grid<bool>, area: Area) -> usize {
-    grid.iter(area)
-        .filter_map(snd)
-        .filter(|&x| *x == true)
-        .count()
+    Ok(Grid::from_iter(Dimensions(100, 100), data)?)
 }
 
 pub fn solve_part_1(input: &str) -> SolverResult {
-    let mut grid = Grid::from_lines(input.lines(), parse_cell)?;
-    let area = Area::square(100);
+    let grid = parse_grid(input)?;
+    let grid = repeat_apply(100, grid, |grid| next_state(grid.view()));
 
-    for _ in 0..100 {
-        animate(&mut grid, area.clone());
-    }
-
-    let lights = count_lights(&grid, area.clone());
-
+    let lights = count_lights(&grid.view());
     Ok(Box::new(lights))
 }
 
 pub fn solve_part_2(input: &str) -> SolverResult {
-    let mut grid = Grid::from_lines(input.lines(), parse_cell)?;
-    let area = Area::square(100);
+    let grid = parse_grid(input)?;
 
-    for _ in 0..100 {
-        animate_broken_corners(&mut grid, area.clone());
-    }
+    let mut grid = repeat_apply(100, grid, |mut grid| {
+        fix_corners(&mut grid.view_mut());
+        next_state(grid.view())
+    });
 
-    fix_corners(&mut grid, &area);
-    let lights = count_lights(&grid, area);
+    fix_corners(&mut grid.view_mut());
+
+
+    let lights = count_lights(&grid.view());
 
     Ok(Box::new(lights))
 }
