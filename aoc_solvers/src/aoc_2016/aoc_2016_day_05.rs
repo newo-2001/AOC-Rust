4dc897c87cf654b445c8fc63c5ee16d35bfa63b6
newo-2001 <o.elderbroek@gmail.com>
2023-09-05 19:59:43 +0200

    Speed up hashing puzzles using parallel iterators

--- aoc_solvers/src/aoc_2016/aoc_2016_day_05.rs
+++ aoc_solvers/src/aoc_2016/aoc_2016_day_05.rs
@@ -1,22 +1,65 @@
+use std::sync::Arc;
+
 use aoc_runner_api::SolverResult;
 use hex::ToHex;
 use itertools::Itertools;
+use rayon::prelude::{ParallelBridge, ParallelIterator, IntoParallelIterator};
 use tupletools::snd;
 
-fn password<'a, T>(door_id: &'a str, extractor: impl Fn(&str) -> Option<T> + 'a) -> impl Iterator<Item=T> + 'a {
-    (0..).filter_map(move |index| {
-        let data = format!("{}{}", door_id, index);
+struct PasswordIterator<'a, T, F>
+    where F: Fn(&str) -> Option<T> + 'a
+{
+    index: u32,
+    door_id: &'a str,
+    extractor: Arc<F>
+}
+
+impl<'a, T, F> Iterator for PasswordIterator<'a, T, F>
+    where F: Fn(&str) -> Option<T> + 'a,
+          Arc<F>: Sync,
+          T: Send
+{
+    type Item = T;
+
+    // Using parallel iterators speeds this process up significantly
+    // Because the order is not deterministic without an upper bound
+    // We need to find that first and then check again using that bound
+    // This is still faster than iterating once sequentially on modern hardware
+    fn next(&mut self) -> Option<Self::Item> {
+        let bound = (self.index..).par_bridge()
+            .find_map_any(|index| self.hash(index).and(Some(index)))
+            .unwrap();
+        
+        let digit = (self.index..=bound).into_par_iter()
+            .find_map_first(|index| self.hash(index))
+            .unwrap();
+        
+        self.index = bound + 1;
+        Some(digit)
+    }
+}
+
+impl<'a, T, F> PasswordIterator<'a, T, F>
+    where F: Fn(&str) -> Option<T> + 'a
+{
+    fn hash(&self, index: u32) -> Option<T> {
+        let data = format!("{}{}", self.door_id, index);
+
         let digest: String = md5::compute(data).encode_hex();
         digest.starts_with("00000")
-            .then(|| extractor(&digest))
+            .then(|| (self.extractor)(&digest))
             .flatten()
-    })
+    }
+
+    fn new(door_id: &'a str, extractor: F) -> PasswordIterator<'a, T, F> {
+        PasswordIterator { index: 0, door_id, extractor: Arc::new(extractor) }
+    }
 }
 
 pub fn solve_part_1(input: &str) -> SolverResult {
     let extractor = |hash: &str| hash.chars().nth(5);
 
-    let password: String = password(input, extractor)
+    let password: String = PasswordIterator::new(input, extractor)
         .take(8)
         .collect();
 
@@ -31,7 +74,7 @@ pub fn solve_part_2(input: &str) -> SolverResult {
         Some((index, char))
     }
 
-    let password: String = password(input, extractor)
+    let password: String = PasswordIterator::new(input, extractor)
         .unique_by(|&(index, _)| index)
         .into_iter()
         .take(8)
