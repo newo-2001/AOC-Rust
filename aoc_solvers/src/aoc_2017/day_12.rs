ae799babd2bfc2ec8bec6e322a937adb2d384b00
newo-2001 <o.elderbroek@gmail.com>
2023-09-22 15:27:42 +0200

    2017 Day 12 Part 1

---
+++ aoc_solvers/src/aoc_2017/day_12.rs
@@ -0,0 +1,26 @@
+use aoc_lib::parsing::{Runnable, ParseError};
+use aoc_runner_api::SolverResult;
+use itertools::Itertools;
+use nom::{character::complete::{u32, line_ending}, bytes::complete::tag, sequence::separated_pair, multi::separated_list0, Parser};
+use petgraph::{Graph, Undirected, visit::{Bfs, Walker}, adj::NodeIndex};
+
+fn parse_graph(input: &str) -> Result<Graph<(), (), Undirected>, ParseError> {
+    let edges = separated_list0(tag(", "), u32);
+    let node = separated_pair(u32, tag(" <-> "), edges)
+        .map(|(id, edges)| edges.into_iter().map(move |to| (id, to)));
+    
+    separated_list0(line_ending, node)
+        .map(|nodes| {
+            let edges = nodes.into_iter().flatten().collect_vec();
+            Graph::from_edges(edges)
+        }).run(input)
+}
+
+pub fn solve_part_1(input: &str) -> SolverResult {
+    let graph = parse_graph(input)?;
+    let subgraph_size = Bfs::new(&graph, NodeIndex::from(0))
+        .iter(&graph)
+        .count();
+
+    Ok(Box::new(subgraph_size))
+}
