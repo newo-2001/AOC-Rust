cac0d76a26ef90f67eeffd32748a5ef14a6deeab
newo-2001 <o.elderbroek@gmail.com>
2023-10-20 13:43:27 +0200

    Refactor 2015 Day 2

--- aoc_solvers/src/aoc_2015/day_02.rs
+++ aoc_solvers/src/aoc_2015/day_02.rs
@@ -1,51 +1,67 @@
-use std::num::ParseIntError;
-use aoc_lib::parsing::parse_lines;
+use aoc_lib::parsing::{lines, Map3, Parsable, TextParserResult, TextParser};
 use aoc_runner_api::SolverResult;
 use itertools::Itertools;
+use nom::{character::complete::{char, u32}, sequence::{tuple, preceded}, Parser};
 
-type Present := Vec<u32>;
+struct Present(u32, u32, u32);
 
-fn parse_present(str: &str) -> Result<Present, ParseIntError> {
-    str.split('x')
-        .map(str::parse::<u32>)
-        .collect::<Result<Vec<u32>, ParseIntError>>()
+impl Parsable for Present {
+    fn parse(input: &str) -> TextParserResult<Present> {
+        tuple((
+            u32,
+            preceded(char('x'), u32),
+            preceded(char('x'), u32)
+        )).map3(Present)
+            .parse(input)
+    }
 }
 
-fn required_wrapping_paper(present: &Present) -> u32 {
-    let areas: Vec<u32> = present.iter()
-        .combinations(2)
-        .map(|sides| sides.into_iter().product())
-        .collect();
+impl IntoIterator for &Present {
+    type Item = u32;
+    type IntoIter = core::array::IntoIter<u32, 3>;
 
-    let min_area: &u32 = areas.iter().min().unwrap_or(&0);
-    let total_area: u32 = areas.iter().sum::<u32>() * 2;
-    min_area + total_area
+    fn into_iter(self) -> Self::IntoIter {
+        [self.0, self.1, self.2].into_iter()
+    }
 }
 
-fn required_ribbon(present: &Present) -> u32 {
-    let smallest_perimeter: u32 = present.iter()
-        .combinations(2)
-        .map(|sides| sides.into_iter().sum::<u32>() * 2)
-        .min().unwrap_or(0);
+impl Present {
+    fn required_wrapping_paper(&self) -> u32 {
+        let areas: Vec<u32> = self.into_iter()
+            .combinations(2)
+            .map(|sides| sides.into_iter().product())
+            .collect();
 
-    let volume: u32 = present.iter().product();
-    
-    smallest_perimeter + volume
+        let min_area: &u32 = areas.iter().min().unwrap_or(&0);
+        let total_area = areas.iter().sum::<u32>() * 2;
+        min_area + total_area
+    }
+
+    fn required_ribbon(&self) -> u32 {
+        let smallest_perimeter: u32 = self.into_iter()
+            .combinations(2)
+            .map(|sides| sides.into_iter().sum::<u32>() * 2)
+            .min().unwrap_or(0);
+
+        let volume: u32 = self.into_iter().product();
+        
+        smallest_perimeter + volume
+    }
 }
 
 pub fn solve_part_1(input: &str) -> SolverResult {
-    let wrapping_paper: u32 = parse_lines(parse_present, input)?
+    let wrapping_paper: u32 = lines(Present::parse).run(input)?
         .iter()
-        .map(required_wrapping_paper)
+        .map(Present::required_wrapping_paper)
         .sum();
 
     Ok(Box::new(wrapping_paper))
 }
 
 pub fn solve_part_2(input: &str) -> SolverResult {
-    let ribbon: u32 = parse_lines(parse_present, input)?
+    let ribbon: u32 = lines(Present::parse).run(input)?
         .iter()
-        .map(required_ribbon)
+        .map(Present::required_ribbon)
         .sum();
 
     Ok(Box::new(ribbon))
