5d5fe010bc615ff11945fb78c6e1fd29a24f09dc
newo-2001 <o.elderbroek@gmail.com>
2023-12-17 16:01:18 +0100

    2023 Day 17 Handle the case where there is no solution

--- aoc_solvers/src/aoc_2023/day_17.rs
+++ aoc_solvers/src/aoc_2023/day_17.rs
@@ -3,6 +3,7 @@ use std::{iter::once, collections::BinaryHeap, cmp::min, hash::Hash};
 use ahash::{HashSet, HashSetExt};
 use aoc_lib::{geometry::{CardinalDirection, Point2D, grid::{Grid, GridLike}, Directional, RotationDirection}, parsing::InvalidTokenError};
 use aoc_runner_api::SolverResult;
+use anyhow::{Result, Context};
 
 #[derive(Clone, Copy)]
 struct Tile(u32);
@@ -57,13 +58,14 @@ impl Ord for State {
             .cmp(&other.location.manhattan_distance(other.goal))
             .reverse()
             .then_with(|| self.distance.cmp(&other.distance))
+            .reverse()
             .then_with(|| self.location.0.cmp(&other.location.0))
             .then_with(|| self.location.1.cmp(&other.location.1))
             .then_with(|| self.since_turn.cmp(&other.since_turn))
             .then_with(|| {
                 directions.iter().position(|&dir| dir == self.direction).unwrap()
                     .cmp(&directions.iter().position(|&dir| dir == other.direction).unwrap())
-            }).reverse()
+            })
     }
 }
 
@@ -81,7 +83,7 @@ impl State {
     }
 }
 
-fn shortest_distance(grid: &Grid<Tile>, min_turn: u8, max_turn: u8) -> u32 {
+fn shortest_distance(grid: &Grid<Tile>, min_turn: u8, max_turn: u8) -> Result<u32> {
     let initial_state = State {
         location: Point2D::zero(),
         goal: grid.area().bottom_right(),
@@ -92,10 +94,11 @@ fn shortest_distance(grid: &Grid<Tile>, min_turn: u8, max_turn: u8) -> u32 {
 
     let mut seen = HashSet::<State>::new();
     let mut queue: BinaryHeap<State> = once(initial_state).collect();
-    let mut best = u32::MAX;
+    let mut best: Option<u32> = None;
 
     while let Some(state) = queue.pop() {        
-        if state.distance >= best { continue; }
+        if state.distance >= best.unwrap_or(u32::MAX) { continue; }
+        
         if let Some(seen) = seen.get(&state) {
             if seen.distance <= state.distance { continue; }
         }
@@ -103,7 +106,7 @@ fn shortest_distance(grid: &Grid<Tile>, min_turn: u8, max_turn: u8) -> u32 {
         seen.replace(state);
 
         if state.location == state.goal && state.since_turn >= min_turn {
-            best = min(best, state.distance);
+            best = Some(min(best.unwrap_or(u32::MAX), state.distance));
             continue;
         }
 
@@ -116,19 +119,19 @@ fn shortest_distance(grid: &Grid<Tile>, min_turn: u8, max_turn: u8) -> u32 {
         queue.extend(states);
     }
 
-    best
+    best.context("No path to the goal exists")
 }
 
 pub fn solve_part_1(input: &str) -> SolverResult {
     let grid: Grid<Tile> = Grid::parse(input)?;
-    let distance = shortest_distance(&grid, 0, 2);
+    let distance = shortest_distance(&grid, 0, 2)?;
 
     Ok(Box::new(distance))
 }
 
 pub fn solve_part_2(input: &str) -> SolverResult {
     let grid: Grid<Tile> = Grid::parse(input)?;
-    let distance = shortest_distance(&grid, 3, 9);
+    let distance = shortest_distance(&grid, 3, 9)?;
 
     Ok(Box::new(distance))
 }
