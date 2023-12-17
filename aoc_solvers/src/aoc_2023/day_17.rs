e492f45ad0107ed8df31e6e76ecf5ecc7de17532
newo-2001 <o.elderbroek@gmail.com>
2023-12-17 17:34:31 +0100

    2023 Day 17 Use PriorityQueue instead of BinaryHeap

--- aoc_solvers/src/aoc_2023/day_17.rs
+++ aoc_solvers/src/aoc_2023/day_17.rs
@@ -1,9 +1,10 @@
-use std::{iter::once, collections::BinaryHeap, cmp::min, hash::Hash};
+use std::{iter::once, cmp::min, hash::Hash};
 
-use ahash::{HashSet, HashSetExt};
+use ahash::{HashMap, HashMapExt};
 use aoc_lib::{geometry::{CardinalDirection, Point2D, grid::{Grid, GridLike}, Directional, RotationDirection}, parsing::InvalidTokenError};
 use aoc_runner_api::SolverResult;
 use anyhow::{Result, Context};
+use priority_queue::PriorityQueue;
 
 #[derive(Clone, Copy)]
 struct Tile(u32);
@@ -18,105 +19,89 @@ impl TryFrom<char> for Tile {
     }
 }
 
-#[derive(Clone, Copy)]
+#[derive(Clone, Copy, PartialEq, Eq, Hash)]
 struct State {
     direction: CardinalDirection,
     location: Point2D<usize>,
-    goal: Point2D<usize>,
-    since_turn: u8,
-    distance: u32
+    since_turn: u8
 }
 
-impl Eq for State {}
-impl PartialEq for State {
-    fn eq(&self, other: &Self) -> bool {
-        self.direction == other.direction &&
-        self.location == other.location &&
-        self.since_turn == other.since_turn
-    }
+#[derive(Clone, Copy, PartialEq, Eq)]
+struct Priority {
+    distance: u32,
+    tiles_to_goal: usize
 }
 
-impl Hash for State {
-    fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
-        self.direction.hash(state);
-        self.location.hash(state);
-        self.since_turn.hash(state);
-    }
-}
-
-impl PartialOrd for State {
+impl PartialOrd for Priority {
     fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
         Some(self.cmp(other))
     }
 }
 
-impl Ord for State {
+impl Ord for Priority {
     fn cmp(&self, other: &Self) -> std::cmp::Ordering {
-        let directions = CardinalDirection::all();
-
-        self.location.manhattan_distance(self.goal)
-            .cmp(&other.location.manhattan_distance(other.goal))
+        self.tiles_to_goal.cmp(&other.tiles_to_goal)
             .reverse()
             .then_with(|| self.distance.cmp(&other.distance))
             .reverse()
-            .then_with(|| self.location.0.cmp(&other.location.0))
-            .then_with(|| self.location.1.cmp(&other.location.1))
-            .then_with(|| self.since_turn.cmp(&other.since_turn))
-            .then_with(|| {
-                directions.iter().position(|&dir| dir == self.direction).unwrap()
-                    .cmp(&directions.iter().position(|&dir| dir == other.direction).unwrap())
-            })
     }
 }
 
 impl State {
-    fn next(self, grid: &Grid<Tile>, direction: CardinalDirection, min_turn: u8, max_turn: u8) -> Option<Self> {
+    fn next(self, direction: CardinalDirection, min_turn: u8, max_turn: u8) -> Option<Self> {
         let location = self.location.checked_add::<isize>(direction.direction_vector())?;
-        let distance = self.distance + grid.get(location)?.0;
         let since_turn = if direction == self.direction {
             (self.since_turn < max_turn).then_some(self.since_turn + 1)?
         } else {
             (self.since_turn >= min_turn).then_some(0)?
         };
         
-        Some(State { direction, location, since_turn, distance, goal: self.goal })
+        Some(State { direction, location, since_turn })
     }
 }
 
 fn shortest_distance(grid: &Grid<Tile>, min_turn: u8, max_turn: u8) -> Result<u32> {
-    let initial_state = State {
+    let goal = grid.area().bottom_right();
+    let initial_state = (State {
         location: Point2D::zero(),
-        goal: grid.area().bottom_right(),
         direction: CardinalDirection::East,
         since_turn: 0,
+    }, Priority {
+        tiles_to_goal: Point2D::<usize>::zero().manhattan_distance(goal),
         distance: 0,
-    };
+    });
 
-    let mut seen = HashSet::<State>::new();
-    let mut queue: BinaryHeap<State> = once(initial_state).collect();
+    let mut queue: PriorityQueue<State, Priority, ahash::RandomState> = once(initial_state).collect();
     let mut best: Option<u32> = None;
+    let mut seen = HashMap::<State, u32>::new();
 
-    while let Some(state) = queue.pop() {        
-        if state.distance >= best.unwrap_or(u32::MAX) { continue; }
-        
-        if let Some(seen) = seen.get(&state) {
-            if seen.distance <= state.distance { continue; }
-        }
-        
-        seen.replace(state);
+    while let Some((state, priority)) = queue.pop() {        
+        if priority.distance >= best.unwrap_or(u32::MAX) { continue; }
 
-        if state.location == state.goal && state.since_turn >= min_turn {
-            best = Some(min(best.unwrap_or(u32::MAX), state.distance));
+        if state.location == goal && state.since_turn >= min_turn {
+            best = Some(min(best.unwrap_or(u32::MAX), priority.distance));
             continue;
         }
 
-        let states = [RotationDirection::Left, RotationDirection::Right].map(|direction| {
-            state.next(grid, state.direction.rotate(direction), min_turn, max_turn)
-        }).into_iter()
-            .chain(once(state.next(grid, state.direction, min_turn, max_turn)))
-            .flatten();
+        if let Some(&shortest) = seen.get(&state) {
+            if shortest <= priority.distance { continue; }
+        }
 
-        queue.extend(states);
+        seen.insert(state, priority.distance);
+
+        let states = [RotationDirection::Left, RotationDirection::Right].map(|direction| {
+            state.next(state.direction.rotate(direction), min_turn, max_turn)
+        }).into_iter()
+            .chain(once(state.next(state.direction, min_turn, max_turn)))
+            .flatten()
+            .filter_map(|state| Some((state, Priority {
+                distance: priority.distance + grid.get(state.location)?.0,
+                tiles_to_goal: state.location.manhattan_distance(goal)
+            })));
+
+        for (state, priority) in states {
+            queue.push_increase(state, priority);
+        }
     }
 
     best.context("No path to the goal exists")
